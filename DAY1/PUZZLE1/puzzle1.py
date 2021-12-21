25557c09b444beddc3f78735864962b7b0fb2f6d
Arthur Cespon <70386689+acespon@users.noreply.github.com>
2021-12-21 02:37:36 +0000

    Puzzless 1

--- DAY1/PUZZLE1/puzzle1.py
+++ DAY1/PUZZLE1/puzzle1.py
@@ -1,1 +1,20 @@
-import numpy
+//DAY 1
+
+def depth_increases(measurements):
+    count = 0
+    currentMeasurement = 0
+
+    for measurement in measurements:
+        if measurement > currentMeasurement:
+            count += 1
+        currentMeasurement = measurement
+
+    count -= 1
+    return count
+
+if __name__ == "__main__":
+    with open('puzzleinput.txt') as f:
+    readings = int(f.readlines())
+
+    print("Day 1 - Advent of Code")
+    print("The number of times a depth measurement increases is: " + depth_increases(readings))
