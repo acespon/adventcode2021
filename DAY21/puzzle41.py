3fbf603ec0a835664f1e7c49930af50ad43dd455
Arthur Cespon <acespon@users.noreply.github.com>
2022-01-01 21:16:27 +0000

    Puzzle 41 Working Out

--- DAY21/puzzle41.py
+++ DAY21/puzzle41.py
@@ -1,3 +1,24 @@
+import numpy as np
+
+with open("puzzleinput.txt") as f:
+    input = f.readlines()
+
+positions = [int(line.split(" ")[-1]) for line in input]
+
+print(positions)
+
+
+dice = 1
+
+def roll_dice():
+    spaces = (dice*3) + 3
+    dice += 2
+    
+    return spaces
+    
+roll_dice()
+print(dice)
+    
 
 
 
